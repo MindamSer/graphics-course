3f9937a6030a7c13e38b3ce54fb2ef714a72efee
MindamSer <androsovy.mail.ru@gmail.com>
2025-06-05 17:02:16 +0300

    baked models

--- tasks/app_renderer/App.cpp
+++ tasks/app_renderer/App.cpp
@@ -19,7 +19,7 @@ App::App()
   auto surface = mainWindow->createVkSurface(etna::get_context().getInstance());
   renderer->initFrameDelivery(std::move(surface), [this]() { return mainWindow->getResolution(); });
 
-  renderer->loadScene(GRAPHICS_COURSE_RESOURCES_ROOT "/scenes/avocado/Avocado.gltf");
+  renderer->loadScene(GRAPHICS_COURSE_RESOURCES_ROOT "/scenes/avocado/Avocado_baked.gltf");
 }
 
 void App::run()
